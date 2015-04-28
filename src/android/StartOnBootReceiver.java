687a49533f005eb843fe8b87d48721b8bfdf7fa7
Tomasz Subik <tsubik@gmail.com>
2015-04-28 10:34:35 +0200

    better resolving activity name

--- src/android/StartOnBootReceiver.java
+++ src/android/StartOnBootReceiver.java
@@ -2,8 +2,13 @@ package com.tsubik.cordova.start_on_boot;
 
 import android.content.Intent;
 import android.content.BroadcastReceiver;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.util.Log;
 import android.content.Context;
+
+import java.util.List;
 
 public class StartOnBootReceiver extends BroadcastReceiver {
     public static final String TAG = "StartOnBootPlugin";
@@ -11,7 +16,7 @@ public class StartOnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
-            String mainActivityName = context.getPackageName() + ".MainActivity";
+            String mainActivityName = getMainActivityName(context);
             Intent serviceIntent = new Intent(context, Class.forName(mainActivityName));
             serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             Log.d(TAG, "Starting " + mainActivityName + " on boot");
@@ -19,5 +24,18 @@ public class StartOnBootReceiver extends BroadcastReceiver {
         } catch(Exception ex) {
             Log.d(TAG, "Cannot start app on boot. Exception" + ex.toString());
         }
+    }
+
+    private String getMainActivityName (Context context) {
+        try {
+            PackageManager pm = context.getPackageManager();
+
+            ActivityInfo[] activities = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES).activities;
+            if (activities.length > 0) {
+                return activities[0].name;
+            }
+        } catch (PackageManager.NameNotFoundException e) {}
+        //return default name
+        return context.getPackageName() + ".MainActivity";
     }
 }
